c9a8244b5c92e7afd18942c2f112c6a1bf163f7e
moisse <moisesapaza07@gmail.com>
2016-03-15 02:07:33 -0300

    start with material design

---
+++ app/src/main/java/com/apaza/moises/practicematerialdesign/model/Place.java
@@ -0,0 +1,106 @@
+package com.apaza.moises.practicematerialdesign.model;
+
+
+import com.apaza.moises.practicematerialdesign.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Place {
+    private String name;
+    private String description;
+    private String address;
+    private double rating;
+    private int pathImage;
+
+    public Place(String name, String description, String addres, double rating, int pathImage){
+        this.name = name;
+        this.description = description;
+        this.address = addres;
+        this.rating = rating;
+        this.pathImage = pathImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Place setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Place setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Place setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public Place setRating(float rating) {
+        this.rating = rating;
+        return this;
+    }
+
+    public int getPathImage() {
+        return pathImage;
+    }
+
+    public Place setPathImage(int pathImage) {
+        this.pathImage = pathImage;
+        return this;
+    }
+
+    private static List<Place> listPlaces = new ArrayList<Place>();
+    private static List<Place> listFoods = new ArrayList<Place>();
+    private static List<Place> listMuseums = new ArrayList<Place>();
+
+    public List<Place> getListPlaces(){
+        listPlaces.add(new Place("Parque cretasico", "Lugar donde se puede ver dinosaurios", "Ruta 7 zona Fancesa", 4.2, R.drawable.places_tourist));
+        listPlaces.add(new Place("La recoleta", "Descripcion de la recoleta", "Av brasil 1000", 4.2, R.drawable.places_tourist));
+        listPlaces.add(new Place("Cementerio general", "Descripcion del cementerio", "Calle colon 123", 4.2, R.drawable.places_tourist));
+        listPlaces.add(new Place("La casa de la libertad", "Descripcion de la casa", "Ruta 7 zona Fancesa", 4.2, R.drawable.places_tourist));
+        listPlaces.add(new Place("Tarabuco", "Descripcion de tarabuco", "Ruta 7 zona Fancesa", 4.2, R.drawable.places_tourist));
+        listPlaces.add(new Place("Yotala", "Lugar donde se puede ver dinosaurios", "Ruta 7 zona Fancesa", 4.2, R.drawable.places_tourist));
+        listPlaces.add(new Place("Potolo", "Lugar donde se puede ver dinosaurios", "Ruta 7 zona Fancesa", 4.2, R.drawable.places_tourist));
+        return listPlaces;
+    }
+
+    public List<Place> getListFoods(){
+        listFoods.add(new Place("Restaurant 7 lunares", "Lugar donde se vende choripan", "Calle loa 1324", 4.2, R.drawable.restaurant));
+        listFoods.add(new Place("Restaurant ", "Lugar donde se vende choripan", "Calle loa 1324", 4.2, R.drawable.restaurant));
+        listFoods.add(new Place("Restaurant ", "Lugar donde se vende choripan", "Calle loa 1324", 4.2, R.drawable.restaurant));
+        listFoods.add(new Place("Restaurant ", "Lugar donde se vende choripan", "Calle loa 1324", 4.2, R.drawable.restaurant));
+        listFoods.add(new Place("Restaurant ", "Lugar donde se vende choripan", "Calle loa 1324", 4.2, R.drawable.restaurant));
+        listFoods.add(new Place("Restaurant ", "Lugar donde se vende choripan", "Calle loa 1324", 4.2, R.drawable.restaurant));
+        listFoods.add(new Place("Restaurant ", "Lugar donde se vende choripan", "Calle loa 1324", 4.2, R.drawable.restaurant));
+        return listFoods;
+    }
+
+    public List<Place> getListMuseums(){
+        listMuseums.add(new Place("Museum 123", "Museo donde nacio la libertad de Bolivia", "Placa 25 de mayo", 4.2, R.drawable.museum));
+        listMuseums.add(new Place("Museum ", "Museo donde nacio la libertad de Bolivia", "Placa 25 de mayo", 4.2, R.drawable.museum));
+        listMuseums.add(new Place("Museum ", "Museo donde nacio la libertad de Bolivia", "Placa 25 de mayo", 4.2, R.drawable.museum));
+        listMuseums.add(new Place("Museum ", "Museo donde nacio la libertad de Bolivia", "Placa 25 de mayo", 4.2, R.drawable.museum));
+        listMuseums.add(new Place("Museum ", "Museo donde nacio la libertad de Bolivia", "Placa 25 de mayo", 4.2, R.drawable.museum));
+        listMuseums.add(new Place("Museum ", "Museo donde nacio la libertad de Bolivia", "Placa 25 de mayo", 4.2, R.drawable.museum));
+        listMuseums.add(new Place("Museum ", "Museo donde nacio la libertad de Bolivia", "Placa 25 de mayo", 4.2, R.drawable.museum));
+
+        return listPlaces;
+    }
+}
